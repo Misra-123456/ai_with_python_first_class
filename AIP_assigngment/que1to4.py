2f4c29ee6cf73db13feb7ef4c73211de17069bec
Misra-123456 <misrashaikh1234@gmail.com>
2025-09-03 19:50:38 +0300

    3rd done

--- AIP_assigngment/que1to4.py
+++ AIP_assigngment/que1to4.py
@@ -23,9 +23,33 @@ while True:
         print(shopping_list)
     elif option == "2":
         print(f"There are {len(shopping_list)}items in the list.")
-        item = input("enter item: ")
-        shopping_list.pop(item)
+        ind = int(input("enter item index: "))
+        if 0 >= ind < len(shopping_list):
+            shopping_list.pop(ind)
+        print(f"your list is: {shopping_list}")
     elif option == "3":
+        print("The following items remain in the list:")
+        print(shopping_list)
         break
     else:
         print("invalid input")
+
+
+
+prices = [10, 14, 22, 33, 44, 13, 22, 55, 66, 77]
+totalsum = 0
+while True:
+    product = int(input("Please select product (1-10) 0 to Quit: "))
+    if product == 0:
+        break
+    elif 1 <= product <= 10:
+        price = prices[product - 1]
+        totalsum += price
+        print(f"Product: {product} Price: {price}")
+    else:
+        print("Invalid selection.")
+print(f"Total: {totalsum}")
+payment = int(input("Payment: "))
+change = payment - totalsum
+print(f"Change: {change}")
+
